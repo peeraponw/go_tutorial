839d948c9bc2f2fab71c1cbd7c86238c68f10f36
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2022-03-06 15:24:44 +0700

    add loop and condition functions

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(greetings("Putin"))
-	fmt.Println(greetingsWithAge("Putin", 45))
-	fmt.Println(greetingsWithAgeAndDrink("Putin", 45, "Vodka"))
+	// fmt.Println("Hello, World!")
+	// fmt.Println(greetings("Putin"))
+	// fmt.Println(greetingsWithAge("Putin", 45))
+	// fmt.Println(greetingsWithAgeAndDrink("Putin", 45, "Vodka"))
+	fmt.Println(sumOfFirst(3))
+	fmt.Println(isPrime(1))
+	fmt.Println(isPrime(2))
+	fmt.Println(isPrime(3))
+	fmt.Println(isPrime(4))
 }
 
 func greetings(name string) string {
@@ -19,4 +24,24 @@ func greetingsWithAge(name string, age uint) string {
 
 func greetingsWithAgeAndDrink(name string, age uint, drink string) string {
 	return "Hello " + name + ". You are " + fmt.Sprint(age) + " years old. Your favorite drink is " + drink + "."
+}
+
+func sumOfFirst(n int) int {
+	var s int = 0
+	for i := 0; i <= n; i++ {
+		s += i
+	}
+	return s
+}
+
+func isPrime(n int) bool {
+	if n == 1 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
