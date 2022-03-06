9c9cb4da53777c30416af9020234051dd5c4b843
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2022-03-06 15:00:14 +0700

    add helloworld and function io

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(greetings("Putin"))
+	fmt.Println(greetingsWithAge("Putin", 45))
+	fmt.Println(greetingsWithAgeAndDrink("Putin", 45, "Vodka"))
+}
+
+func greetings(name string) string {
+	return "Hello " + name
+}
+
+func greetingsWithAge(name string, age uint) string {
+	return "Hello " + name + ". You are " + fmt.Sprint(age) + " years old."
+}
+
+func greetingsWithAgeAndDrink(name string, age uint, drink string) string {
+	return "Hello " + name + ". You are " + fmt.Sprint(age) + " years old. Your favorite drink is " + drink + "."
 }
